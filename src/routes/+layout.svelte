091a3a983aa9630e070d1bcbbabdf069c75c5b28
elbi3 <ellbi3@fastmail.com>
2024-05-05 10:17:28 -0700

    add Footer component to layout and components dir

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
     import "$lib/main.scss";
     import Header from "$lib/components/Header.svelte";
+    import Footer from "$lib/components/Footer.svelte";
 </script>
 
 <div>
     <Header/>
     <slot/>
+    <Footer/>
 </div>
