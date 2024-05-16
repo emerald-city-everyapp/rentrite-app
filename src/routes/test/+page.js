0241152a4b58e928635837762d8e7b7579cf9e04
elbi3 <ellbi3@fastmail.com>
2024-05-16 11:48:49 -0700

    fix svelte config from vite

--- src/routes/test/+page.js
+++ src/routes/test/+page.js
@@ -1,12 +1,13 @@
 import { error } from "@sveltejs/kit";
+const apiKey = process.env.API_KEY;
 // be sure to rename file to `+page.server.js` if load function is meant to only run on the server, or to access private env variables like API keys,
 //then change PageLoad to ServerPageLoad (types)
 
 export async function load({ fetch }) {
-    const json = await fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    const content = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
     // const json = await fetch('https://rentrite-backend-53dxbcm3gq-uc.a.run.app/greeting')
-    // console.log(json.json());
-    return json.json();
+    console.log(content.json());
+    return content.json();
 }
 
 //reference: https://kit.svelte.dev/docs/types#public-types-loadevent
