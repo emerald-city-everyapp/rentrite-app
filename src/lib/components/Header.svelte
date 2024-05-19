6980e8b89d727c2dfbe0df187c185fa33b1e6136
elbi3 <ellbi3@fastmail.com>
2024-05-19 10:01:48 -0700

    update header component with nav styling

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -5,7 +5,16 @@
 <header class="site-header" data-type="wide">
     <div class="container">
         <div class="site-header__inner">
-            <h1>{siteTitle}</h1>
+            <div class="site-logo">
+                <a href="/">LOGO</a>
+                <h1>{siteTitle}</h1>
+              </div>
+              <nav class="header-nav">
+                  <a href="/share">share</a>
+                  <a href="/contact">contact</a>
+                  <a href="/login">login</a>
+                  <a href="/test">test</a>
+              </nav>
         </div>
     </div>
 </header>
