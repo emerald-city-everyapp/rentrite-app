9fc50387c7af3911c3c952af39d9a7d1f54ff566
elbi3 <ellbi3@fastmail.com>
2024-06-14 08:41:16 -0700

    add spacing for nav link subgrids

--- src/lib/components/HeaderNav.svelte
+++ src/lib/components/HeaderNav.svelte
@@ -3,19 +3,19 @@
     import inkblot2 from "$lib/inkblot.svg";
 </script>
 
-<nav class="nav">
+<nav class="nav-header">
     <ul class="nav-header-grid">
         <li class="subgrid top">
             <img class="mySvg" src={inkblot} alt="lumpy pilly svg" />                        
-            <a href="/login">login</a>
+            <a class="subgrid__site-link" href="/login">login</a>
         </li>
         <li class="subgrid middle">
             <img class="mySvg" src={inkblot2} alt="lumpy pilly svg" />                        
-            <a href="/contact">contact</a>
+            <a class="subgrid__site-link" href="/contact">contact</a>
         </li>
         <li class="subgrid bottom">
             <img class="mySvg" src={inkblot} alt="lumpy pilly svg" />                       
-            <a href="/share">share</a>
+            <a class="subgrid__site-link" href="/share">share</a>
         </li>
     </ul>
 </nav>
