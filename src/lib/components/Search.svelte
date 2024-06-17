4cb17f3470dfd8a2fa6e22f710bb13b693c323f9
elbi3 <ellbi3@fastmail.com>
2024-06-17 11:06:37 -0700

    refactor search component styling and generalize site wide form styling

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -5,8 +5,8 @@
 <search>
     <form action="get" class="dbsearch">
         <div class="dbsearch-grid">
-            <label class="dbsearch-grid-label" for="lookup">enter an address:</label>
-            <input class="dbsearch-grid-input" type="search" name="lookup">
+            <label class="labels" for="lookup">enter an address:</label>
+            <input class="inputs" type="search" name="lookup">
         </div>
         <button class="button" type="submit" data-type="primary">look it up</button>
     </form>
